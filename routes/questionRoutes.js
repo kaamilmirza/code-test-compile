97949bc2f8cf74ba38b248ee722ef6c5d0542725
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2023-07-14 18:40:32 +0530

    Fixing typos

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -11,7 +11,7 @@ const {
 } = require('../controllers/questionsController');
 const {
   authenticateToken,
-  isAdmin
+  isAdmin,
 } = require('../middleware/authMiddleware');
 const {
     signup,
