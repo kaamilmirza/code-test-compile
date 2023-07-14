bfff3056a92912429e147b579c01ad9b28b94b86
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2023-07-14 18:32:38 +0530

    Adding routes file

---
+++ routes/questionRoutes.js
@@ -0,0 +1,51 @@
+// questionRoutes.js
+
+const express = require('express');
+const router = express.Router();
+const {
+  addQuestion,
+  editQuestion,
+  deleteQuestion,
+  addTestCase,
+  submitSolution
+} = require('../controllers/questionsController');
+const {
+  authenticateToken,
+  isAdmin
+} = require('../middleware/authMiddleware');
+const {
+    signup,
+    login
+
+} = require('../controllers/authController');
+
+// Signup
+router.post('/signup', signup);
+
+// Login
+router.post('/login', login);
+
+// Add a question
+router.post('/questions', authenticateToken, isAdmin, addQuestion);
+
+// Edit a question
+router.put('/questions/:questionId', authenticateToken, isAdmin, editQuestion);
+
+// Delete a question
+router.delete('/questions/:questionId', authenticateToken, isAdmin, deleteQuestion);
+
+// Add a test case to a question
+router.post(
+  '/questions/:questionId/testcases',
+  authenticateToken,
+  isAdmin,
+  addTestCase
+);
+
+router.post(
+    '/questions/:questionId/submit',
+    authenticateToken,
+    submitSolution
+  );
+
+module.exports = router;
